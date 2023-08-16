108455ad654b97bba0846ec9569083ffce2b2919
Abdulqadir <76737223+Abdulqadir8604@users.noreply.github.com>
2023-08-16 21:29:28 +0530

    Create tasbeehData.kt

---
+++ tasbeehData.kt
@@ -0,0 +1,51 @@
+val homeTasbeeh = listOf(
+    "تسبیح",
+)
+
+val ahlebait = listOf(
+    "يا محمد",
+    "يا علي",
+    "يا فاطمة",
+    "يا حسن",
+    "يا حسين",
+    "يا زينب",
+    "يا عباس",
+    "يا طيب",
+)
+
+val impNames = listOf(
+    "يا علي",
+    "يا فاطمة",
+    "يا حسين",
+)
+
+val shortNames = listOf(
+    "تسبیح",
+    "يا محمد",
+    "يا علي",
+    "يا فاطمة",
+    "يا حسن",
+    "يا حسين",
+    "يا زينب",
+    "يا عباس",
+    "يا طيب",
+)
+
+val longTasbeehs = mapOf(
+    "صلوات" to "أللهم صل على محمد و على أل محمد وبارك وسلم",
+)
+
+val tasbeehTypes = listOf(
+    "تسبیح",
+    "صلوات",
+    "أهل البيت",
+)
+
+val singleTasbeeh = listOf(
+    "تسبیح",
+    "صلوات",
+)
+
+val hasSub = mapOf(
+    "أهل البيت" to ahlebait,
+)
